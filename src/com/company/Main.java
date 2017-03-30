14f64560f4b38a55a49de1899c12c9afd081e63e
Alexander Panteleev <746590@mail.ru>
2017-03-31 01:12:53 +0300

    Добавил главное меню

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-    User();
+main_menu();
 
     }
     public static void check(int a, int b){
@@ -36,7 +36,40 @@ public class Main {
         NOD(a,b);
     }
     public static void Random(){
-        
+        int a = (int)(Math.random()*100000);
+        int b = (int)(Math.random()*100000);
+        System.out.println("Первое число " +a);
+        System.out.println("Второе число " +b);
+        NOD(a,b);
+    }
+    public static void main_menu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*******************************************************");
+        System.out.println("Данная программа позволяет найти наибольший общий делитель двух чисел");
+        System.out.println("Для самостоятельного ввода двух чисел нажмите 1");
+        System.out.println("Для выобра случайных чисел в промежутке [0;100000) нажмите 2");
+        System.out.println("*******************************************************");
+        boolean isTrue = true;
+        while (isTrue)
+        {
+            isTrue = false;
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    User();
+                    break;
+                case 2:
+                    Random();
+                    break;
+                default:
+                    System.out.println("Повторите ввод");
+                    isTrue = true;
+                    break;
+            }
+
+        }
+
     }
 
 }
